682f01999c75ab09bd3d9273bde92352af92662b
abiodun <k.akinlotan@yahoo.com>
2022-11-10 08:10:36 +0000

    nav bar added

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
-const card=document.querySelector('.card')
+const card=document.querySelector('.row')
 
 const runPorgram=async ()=>{
     try {
       const response = await fetch('https://striveschool-api.herokuapp.com/books')
       const books = await response.json()
-      .then((books)=>{
         console.log(books)
         card.innerHTML=''
         books.forEach(book => {
            console.log(books)
-           card.innerHTML+=`
-           <div class="container mt-5">
-           <div class="row">
-               <div class="col">
-           <div class="card">
-               <img src="${book.img}" class="card-img-top" alt="..." style="width:200px; height: 200px;">
+           card.innerHTML+=`<div id="cardcontent" class="col-4">
+           <div class="card mt-3">
+               <img src="${book.img}" class="card-img-top" alt="...">
                <div class="card-body">
-                 <h5 class="card-title"> Title: ${book.title}</h5>
+                 <h5 class="card-title">Tittle${book.title}</h5>
                  <p class="card-text">ID:${book.asin}</p>
-                 <p>Price:${book.price}</p>
+                 <p>Price:$ ${book.price}</p>
                  <p>Category:${book.category}</p>
-                 <button id="addbuttn">Add to cart</button>
-                 <button id="addbuttn">skip</button>
+                 <a href="#" id='addbutton'onclick='addCart()' class="btn btn-primary">Add to cart</a>
+                 <a href="#" class="btn btn-primary">skip</a>
                </div>
-               </div>
-               </div>
-             </div> `
-        });
-   })
+             </div>
+         </div>`
+    });
     } catch (error) {
       console.error(error)
     }
 }
-runPorgram()
+const carousel=document.querySelector('.carousel-inner')
+const carouselPic=async ()=>{
+    try {
+      const response = await fetch('https://striveschool-api.herokuapp.com/books')
+      const books = await response.json()
+        console.log(books)
+        carousel.innerHTML=''
+        books.forEach(book => {
+           console.log(books)
+           carousel.innerHTML+=`
+              <div class="carousel-item active">
+                <img src="${book.img}" class="d-block w-100" alt="...">
+              </div>`
+    
+    });
+    } catch (error) {
+      console.error(error)
+    }
+}
 
+carouselPic()
+window.onload=runPorgram
 
+function addCart(){
+ const ul=document.querySelector('#cartAdd')
+ ul.innerHTML+=`<div id="cardcontent" class="col-4">
+ <div class="card mt-3">
+     <img src="${book.img}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">Tittle${book.title}</h5>
+       <p class="card-text">ID:${book.asin}</p>
+       <p>Price:$ ${book.price}</p>
+       <p>Category:${book.category}</p>
+       <a href="#" id='addbutton'class="btn btn-primary">Add to cart</a>
+       <a href="#" class="btn btn-primary">skip</a>
+     </div>
+   </div>
+</div>`
+}
 
